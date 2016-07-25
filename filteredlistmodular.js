049c4d76956b679989625db3a7d6bb624834b876
Laurentiu Iosif Orban <orbanlaurentiu@gmail.com>
2016-07-25 12:41:59 +0300

    initial commit

---
+++ filteredlistmodular.js
@@ -0,0 +1,10 @@
+var myMod=require("./filtermodule.js");
+
+myMod(process.argv[2], process.argv[3], function(err, data){
+    
+    if (err) {return console.log("!!error!!");}
+    for (var i=0; i<data.length; i++) {
+       console.log(data[i]);
+   }
+    
+});
